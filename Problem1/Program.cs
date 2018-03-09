ea919493373dff1a2d6a5dbca77de3254927999b
vgradev <vgradev@yahoo.com>
2018-03-09 19:08:28 +0200

    Add new cs file

---
+++ Problem1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double density = double.Parse(Console.ReadLine());
+            double sum = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+                double raindropsCount = nums[0];
+                double squareMeters = nums[1];
+                sum += raindropsCount / squareMeters;
+            }
+
+            if (sum != 0 && density != 0)
+            {
+                double totalSum = sum / density;
+                Console.WriteLine("{0:f3}", totalSum);
+            }
+            else
+            {
+                Console.WriteLine("{0:f3}", sum);
+            }
+        }
+    }
+}
